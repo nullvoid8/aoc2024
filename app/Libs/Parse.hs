a5e0e9d6b8228239d67ee3de1a2f32915df19d4f
nullvoid8 <nullvoid8@gmail.com>
2024-12-02 11:08:04 +0000

    day1

--- app/Libs/Parse.hs
+++ app/Libs/Parse.hs
@@ -1,60 +1,86 @@
-{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingStrategies, OverloadedStrings, ScopedTypeVariables #-}
 
 module Libs.Parse (
   Parser (..),
   char, newline, takeWhile1, spaces, take,
-  decimal, hexadecimal, signed, rational, double,
+  decimal, hexadecimal, signed, rational, double, runOnce,
 ) where
 
 import Control.Applicative (Alternative (..), asum)
-import Control.Monad ((<=<), void)
+import Data.Functor (void)
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Read qualified as TR
 import Prelude hiding (take)
 
-newtype Parser a = P {runParser :: Text -> [(Text, a)]} deriving (Functor)
+data Result a = Fail Text | Success Text a (Result a) deriving (Functor, Show)
+
+instance Semigroup (Result a) where
+  (<>) :: Result a -> Result a -> Result a
+  Fail xs <> Fail _ = Fail xs
+  Fail _ <> xs = xs
+  Success t x l <> r = Success t x $ l <> r
+
+-- instance Monoid (Result a) where
+--   mempty :: Result a
+--   mempty = Fail "mempty"
+
+newtype Parser a = P {runParser :: Text -> Result a} deriving (Functor)
+
+runOnce :: Parser a -> Text -> Either Text a
+runOnce p t = case runParser p t of
+  Fail err -> Left err
+  Success _ x _ -> Right x
 
 instance Applicative Parser where
-  pure x = P $ \t -> [(t, x)]
-  P pf <*> P px = P $ \t -> do
-    (t', f) <- pf t
-    fmap f <$> px t'
+  pure :: a -> Parser a
+  pure x = P $ \t -> Success t x $ Fail "pure"
+  
+  (<*>) :: forall a b. Parser (a -> b) -> Parser a -> Parser b
+  pf <*> px = P $ \t -> go $ runParser pf t
+    where
+      go :: Result (a -> b) -> Result b
+      go (Fail err) = Fail err
+      go (Success t f fs) = (f <$> runParser px t) <> go fs
 
 instance Alternative Parser where
-  empty = P $ const []
+  empty :: Parser a
+  empty = P $ const $ Fail "empty"
+  
   (<|>) :: Parser a -> Parser a -> Parser a
-  P pa <|> P pb = P $ \t -> pa t ++ pb t
-
-instance Monad Parser where
-  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-  pa >>= f = P $ uncurry (flip runParser) <=< runParser (f <$> pa)
+  pa <|> pb = P $ \t -> runParser pa t <> runParser pb t
 
 -- combinators
+
+failWith :: Text -> Parser a -> Parser a
+failWith msg (P p) = P $ \t -> case p t of
+  (Fail _) -> Fail msg
+  rs -> rs
 
 -- text
 
 char :: Char -> Parser Char
 char c = P $ \t -> case T.uncons t of
   Just (x, t')
-    | c == x -> [(t', c)]
-  _ -> []
+    | c == x -> Success t' x $ Fail "char"
+    | otherwise -> Fail $ "expected '" <> T.pack (show c) <> "' got EOF" <> T.pack (show x) <> "'"
+  _ -> Fail $ "expected '" <> T.pack (show c) <> "' got EOF"
 
 newline :: Parser ()
 newline = void $ char '\n'
 
 liftTextPair :: (Text -> (Text, Text)) -> Parser Text
 liftTextPair f = P $ \t -> case f t of
-  (l, r) -> [(l, r)]
+  (l, r) -> Success r l $ Fail "liftTextPair"
 
 liftTextPair1 :: (Text -> (Text, Text)) -> Parser Text
 liftTextPair1 f = P $ \t -> case f t of
   (l, r)
-    | T.null l -> empty
-    | otherwise -> [(l, r)]
+    | T.null l -> Fail "liftTextPair1"
+    | otherwise -> Success r l $ Fail "liftTextPair1"
 
 takeWhile1 :: (Char -> Bool) -> Parser Text
-takeWhile1 p = liftTextPair1 $ T.span p
+takeWhile1 p = failWith "takeWhile1"$ liftTextPair1 $ T.span p
 
 spaces :: Parser ()
 spaces = void (takeWhile1 (== ' '))
@@ -66,8 +92,8 @@ take i = liftTextPair $ T.splitAt i
 
 liftTextReader :: TR.Reader a -> Parser a
 liftTextReader r = P $ \t -> case r t of
-  Left _ -> empty
-  Right (x, t') -> [(t', x)]
+  Left err -> Fail $ T.pack err
+  Right (x, t') -> Success t' x $ Fail "liftTextReader"
 
 decimal :: (Integral a) => Parser a
 decimal = liftTextReader TR.decimal
@@ -76,11 +102,10 @@ hexadecimal :: (Integral a) => Parser a
 hexadecimal = liftTextReader TR.hexadecimal
 
 signed :: (Num a) => Parser a -> Parser a
-signed p = do
-  sign <- asum [char '-', char '+', pure '+']
-  case sign of
-    '-' -> negate <$> p
-    _ -> p
+signed p = signer <$> asum [char '-', char '+', pure '+'] <*> p
+  where
+    signer '-' x = negate x
+    signer _ x = x
 
 rational :: (Fractional a) => Parser a
 rational = liftTextReader TR.rational
--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,26 @@ module Main where
 
 import qualified Data.Text.IO as TIO
 import qualified Libs.Parse as P
+import Data.List (sort)
 import Control.Applicative (Alternative(some))
 
 
 main :: IO ()
 main = do
-  input <- P.runParser parser <$> TIO.readFile "input/day1"
-  print input
+  input <- either failShow pure . P.runOnce parser =<< TIO.readFile "input/day1"
+  let xs = sort $ fmap fst input
+  let ys = sort $ fmap snd input
+  print $ sum . fmap abs $ zipWith subtract xs ys
+
+
+  print $ sum $ do
+    x <- xs
+    pure $ x * length (filter (== x) ys)
 
 parser :: P.Parser [(Int, Int)]
 parser = some $ (,) <$> P.decimal <* P.spaces <*> P.decimal <* P.newline
+-- parser = P.decimal <* P.spaces
 
-
+failShow :: Show a => a -> IO b
+failShow = fail . show
 
