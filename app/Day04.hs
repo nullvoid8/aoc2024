f1209bea03d09c5c09f6e09fa2714074b8aa6158
nullvoid8 <nullvoid8@gmail.com>
2024-12-04 15:17:44 +0000

    day 4 part 2

--- app/Day04.hs
+++ app/Day04.hs
@@ -17,6 +17,8 @@ main = do
   let neg = getSum . foldMap (Sum . countXmas) $ negDiags input
 
   print $ rows + cols + pos + neg
+
+  print $ p2 input
   -- print $ getSum $ rows + cols
 
 countXmas :: Text -> Int
@@ -35,3 +37,19 @@ negDiags xs = T.transpose $ zipWith3 (\a b t -> a <> t <> b) before after xs whe
   len = T.length (head xs)
   before = T.inits $ T.replicate len "."
   after = T.tails $ T.replicate len "."
+
+p2 :: [Text] -> Int
+p2 = go 0 where
+  go :: Int -> [Text] -> Int
+  go !acc (top:xs@(mid:bot:_)) = go (acc + go' top mid bot) xs
+  go !acc _ = acc
+
+  go' :: Text -> Text -> Text -> Int
+  go' top mid bot = sum $ zipWith3 match (T.unpack . T.take 3 <$> T.tails top) (drop 1 . T.unpack $ mid) (T.unpack . T.take 3 <$> T.tails bot)
+
+  match :: String -> Char -> String -> Int
+  match ('M':_:'M':_) 'A' ('S':_:'S':_) = 1
+  match ('M':_:'S':_) 'A' ('M':_:'S':_) = 1
+  match ('S':_:'M':_) 'A' ('S':_:'M':_) = 1
+  match ('S':_:'S':_) 'A' ('M':_:'M':_) = 1
+  match _ _ _ = 0
