ad151d278f1a6789203bc3326ebc9170bc003104
nullvoid8 <nullvoid8@gmail.com>
2024-12-07 14:59:44 +0000

    improve d7 by working backwards

--- app/Day07.hs
+++ app/Day07.hs
@@ -5,32 +5,39 @@ module Main where
 import Libs.Parse (Parser)
 import Libs.Parse qualified as P
 import Data.Text.IO qualified as TIO
+import Data.Maybe (catMaybes, mapMaybe)
 
 main :: IO ()
 main = do
     input <- P.runOnceIO parser =<< TIO.readFile "input/day07"
-    print $ sum . fmap fst . filter (\(g, xs) -> elem g $ allEvals p1Ops xs) $ input
-    print $ sum . fmap fst . filter (\(g, xs) -> elem g $ allEvals p2Ops xs) $ input
+    print $ sum . fmap fst . filter (uncurry (canAchieve p1Ops)) $ input
+    print $ sum . fmap fst . filter (uncurry (canAchieve p2Ops)) $ input
 
 parser :: Parser [(Int, [Int])]
 parser = P.lines $ (,) <$> P.decimal <* P.text ": " <*> P.decimal `P.sepBy` P.spaces
-
-allEvals :: [Op] -> [Int] -> [Int]
-allEvals ops = go [] where
-  go :: [Int] -> [Int] -> [Int]
-  go !acc (x:y:xs) = foldr (\op acc -> go acc (eval op x y:xs)) acc ops
-  go !acc [x] = x : acc
-  go _ [] = error "ran out of numbers"
-
 
 data Op = Add | Mul | Cat deriving Show
 
 p1Ops :: [Op]
 p1Ops = [Add, Mul]
 
+p2Ops :: [Op]
 p2Ops = [Add, Mul, Cat]
 
-eval :: Op -> Int -> Int -> Int
-eval Add x y = x + y
-eval Mul x y = x * y
-eval Cat x y = (x * (10 ^ length (show y))) + y
+uneval :: Op -> Int -> Int -> Maybe Int
+uneval Add tgt x
+  | tgt >= x = Just $ tgt - x
+uneval Mul tgt x
+  | tgt `rem` x == 0 = Just $ tgt `div` x
+uneval Cat tgt x
+  | x < 10 && tgt `rem` 10 == x = Just $ tgt `div` 10
+  | x < 100 && tgt `rem` 100 == x = Just $ tgt `div` 100
+  | x < 1000 && tgt `rem` 1000 == x = Just $ tgt `div` 1000
+uneval _ _ _ = Nothing
+
+canAchieve :: [Op] -> Int -> [Int] -> Bool
+canAchieve ops goal xs = go (reverse xs) goal where
+  go :: [Int] -> Int -> Bool
+  go [] 0 = True
+  go (x:xs) tgt = any (go xs) $ mapMaybe (\op -> uneval op tgt x) ops
+  go _ _ = False
