f867bad27a6f78f8455ae79b452905a9d0b95447
nullvoid8 <nullvoid8@gmail.com>
2024-12-10 20:43:06 +0000

    refactor common logic day 10

--- app/Day10.hs
+++ app/Day10.hs
@@ -16,9 +16,8 @@ import Data.Foldable
 main :: IO ()
 main = do
   input <- P.runOnceIO parser =<< TIO.readFile "input/day10"
-  print $ trailScore input
-  print $ trailRating input
-
+  print $ foldTrails S.singleton (sum . fmap S.size) input
+  print $ foldTrails (const 1) (getSum . fold) input
 
 type Point = V2 Int
 type Grid a = M.Map Point a
@@ -26,32 +25,17 @@ type Grid a = M.Map Point a
 parser :: Parser (Grid Int)
 parser = P.grid P.newline (read . (:[]) <$> P.satisfies isDigit) M.singleton
 
-trailScore :: Grid Int -> Int
-trailScore g = sum . fmap S.size . M.elems $ M.restrictKeys trailMap groundKeys where
+foldTrails :: forall a. Monoid a => (Point -> a) -> ([a] -> Int) -> Grid Int -> Int
+foldTrails score9 final g = final . M.elems $ M.restrictKeys trailMap groundKeys where
   groundKeys :: S.Set Point
   groundKeys = M.keysSet $ M.filter (== 0) g
 
   neighbours :: Point -> [Point]
   neighbours p = [p + V2 0 1, p + V2 1 0, p - V2 1 0, p - V2 0 1]
 
-  trail :: Point -> Int -> S.Set Point
-  trail p 9 = S.singleton p
+  trail :: Point -> Int -> a
+  trail p 9 = score9 p
   trail p h = foldMap (trailMap M.!) $ filter (\q -> M.findWithDefault 0 q g - h == 1) (neighbours p)
 
-  trailMap :: Grid (S.Set Point)
-  trailMap = M.mapWithKey trail g
-
-trailRating :: Grid Int -> Int
-trailRating g = getSum . fold $ M.restrictKeys trailMap groundKeys where
-  groundKeys :: S.Set Point
-  groundKeys = M.keysSet $ M.filter (== 0) g
-
-  neighbours :: Point -> [Point]
-  neighbours p = [p + V2 0 1, p + V2 1 0, p - V2 1 0, p - V2 0 1]
-
-  trail :: Point -> Int -> Sum Int
-  trail _ 9 = 1
-  trail p h = foldMap (trailMap M.!) $ filter (\q -> M.findWithDefault 0 q g - h == 1) (neighbours p)
-
-  trailMap :: Grid (Sum Int)
+  trailMap :: Grid a
   trailMap = M.mapWithKey trail g
